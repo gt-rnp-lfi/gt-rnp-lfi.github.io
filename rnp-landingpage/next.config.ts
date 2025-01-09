4991edb14eee52675481ee7cacc1cdf9bf89c55a
Felipe Dresch <felipe10dresch12@gmail.com>
2025-01-09 20:27:50 -0300

    Finalização das configurações build/deploy

--- rnp-landingpage/next.config.ts
+++ rnp-landingpage/next.config.ts
@@ -2,8 +2,9 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/rnp-landingpage',
-  assetPrefix: '/rnp-landingpage/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
