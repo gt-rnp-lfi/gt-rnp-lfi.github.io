8e6ca184321701f5faff31e2eb337bfd1de7d9c2
Felipe Dresch <felipe10dresch12@gmail.com>
2025-01-09 17:24:31 -0300

    Configurações para deploy

--- rnp-landingpage/next.config.ts
+++ rnp-landingpage/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Exporta o site como estático
+  basePath: '/gt-rnp-lfi.github.io', // Substitua pelo nome do repositório
+  assetPrefix: '/gt-rnp-lfi.github.io', // Substitua pelo nome do repositório
 };
 
 export default nextConfig;
